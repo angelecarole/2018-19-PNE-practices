e4968e2b65afb12ca18efc22d6961a0ae1a4f078
angelecarole <angele-carole.biloah-ntsama@hsrw.org>
2019-04-30 23:21:27 +0200

    webserverP5

---
+++ P5FOLDER/webserverP5.py
@@ -0,0 +1,77 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8088
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
+
+        if self.path == "/":
+            filename = "index.html"
+            self.send_response(200)
+        elif self.path == "/blue":
+            self.send_response(200)
+            filename = "blue.html"
+            self.send_response(200)
+        elif self.path == "/pink":
+            filename = "pink.html"
+            self.send_response(200)
+        else:
+            filename = "error.html"
+
+
+
+        with open(filename, "r") as p:
+            contents = p.read()
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
