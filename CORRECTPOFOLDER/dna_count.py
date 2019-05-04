ec46c2d07d51eba6afe22e1093658ea3f7e76a19
angelecarole <angele-carole.biloah-ntsama@hsrw.org>
2019-05-04 14:10:31 +0200

    session3and4

---
+++ CORRECTPOFOLDER/dna_count.py
@@ -0,0 +1,7 @@
+seq = "CATGTAGACTAG"
+dna_count = seq.count("A") + seq.count("T") + seq.count("G") + seq.count("C")
+print(dna_count)
+print(seq.count("A"))
+print(seq.count("T"))
+print(seq.count("G"))
+print(seq.count("C"))
---
+++ CORRECTPOFOLDER/dna_count_file.py
@@ -0,0 +1,9 @@
+my_file = open("dna.txt")
+my_dna = my_file.read()
+
+dna_count = my_dna.count("A") + my_dna.count("T") + my_dna.count("G") + my_dna.count("C")
+print(dna_count)
+print(my_dna.count("A") )
+print(my_dna.count("T"))
+print(my_dna.count("G"))
+print(my_dna.count("C"))
