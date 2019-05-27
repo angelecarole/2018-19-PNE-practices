e80bde0a7bc2ac07ab82cd360ba2fe342a55490a
angelecarole <angele-carole.biloah-ntsama@hsrw.org>
2019-05-27 13:49:53 +0200

    session-9

---
+++ session-9/example-1.py
@@ -0,0 +1,18 @@
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8085
+IP = "192.168.1.37"
+MAX_OPEN_REQUESTS = 5
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP and PORT
+serversocket.bind((IP, PORT))
+
+# Configure the server sockets
+# MAX_OPEN_REQUESTS connect requests before refusing outside connections
+serversocket.listen(MAX_OPEN_REQUESTS)
+
+print("Socket ready: {}".format(serversocket))
