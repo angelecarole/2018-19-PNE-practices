1690f2c05a274efcae3e1b8a630dfd82d1f4e220
angelecarole <angele-carole.biloah-ntsama@hsrw.org>
2019-05-27 15:39:25 +0200

    practice1

--- P1 FOLDER/Seq_main.py
+++ P1 FOLDER/Seq_main.py
@@ -1,41 +1,33 @@
 from Seq import Seq
 
-#  create the sequences
-s1 = Seq("AGTA")
-l1 = s1.len()
-print("Sequence 1: Len: {}".format(l1))
-s2 = s1.complement()
-l2 = s2.len()
-print("Sequence 2: len: {}".format(l2))
-nA = s1.count('A')
-nT = s1.count('T')
-nC = s1.count('C')
-nG = s1.count('G')
-print(" Bases Count: A:{}, T:{}, C:{}, G: {}".format(nA, nT, nC, nG ))
-
-# Count the percentages
-my_out2 = s1.perc()
-print (my_out2)
+sequence1 = Seq('AGTC')
+sequence2 = Seq('GGT')
+sequence3 = sequence1.complement()
+sequence4 = sequence3.reverse()
 
 
-"""
+#Main program
 
-s2 = Seq("GTCCCTTTAACGATTTAG")
-s3 = s1.complement()
-s3 = Seq(s3)
-s4 = s3.reverse()
-s4 = Seq(s4)
+#operation for sequence 1
+print("Sequence 1: ", sequence1.strbase)
+print("  Length: ", sequence1.len())
+print("  Bases count: ", 'A:', sequence1.count('A'), 'G:', sequence1.count('G'), 'T:', sequence1.count('T'), 'C:', sequence1.count('C'))
+print("  Bases percentages: ", 'A:', sequence1.perc('A'), 'G:', sequence1.perc('G'), 'T:', sequence1.perc('T'), 'C:', sequence1.perc('C'))
 
-# This is a list for organising the sequences
-my_seq = [s1, s2, s3, s4]
+#operation for sequence 2
+print("Sequence 2: ", sequence2.strbase)
+print("  Length: ", sequence2.len())
+print("  Bases count: ", 'A:', sequence2.count('A'), 'G:', sequence2.count('G'), 'T:', sequence2.count('T'), 'C:', sequence2.count('C'))
+print("  Bases percentages: ", 'A:', sequence2.perc('A'), 'G:', sequence2.perc('G'), 'T:', sequence2.perc('T'), 'C:', sequence2.perc('C'))
 
-number = 0
+#operation for sequence 3
+print("Sequence 3: ", sequence3.strbase)
+print("  Length: ", sequence3.len())
+print("  Bases count: ", 'A:', sequence3.count('A'), 'G:', sequence3.count('G'), 'T:', sequence3.count('T'), 'C:', sequence3.count('C'))
+print("  Bases percentages: ", 'A:', sequence3.perc('A'), 'G:', sequence3.perc('G'), 'T:', sequence3.perc('T'), 'C:', sequence3.perc('C'))
 
-# Print the statistics requested
-for sequence in my_seq:
-    number += 1
-    print("SEQUENCE: ", number)
-    print("Length: ", sequence.len())
-    print(sequence.count_base())
-    print(sequence.perc())
-"""
+#operation for sequence 4
+print("Sequence 4: ", sequence4.strbase)
+print("  Length: ", sequence4.len())
+print("  Bases count: ", 'A:', sequence4.count('A'), 'G:', sequence4.count('G'), 'T:', sequence4.count('T'), 'C:', sequence4.count('C'))
+print("  Bases percentages: ", 'A:', sequence4.perc('A'), 'G:', sequence4.perc('G'), 'T:', sequence4.perc('T'), 'C:', sequence4.perc('C'))
